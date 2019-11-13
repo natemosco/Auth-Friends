4dbe146d5406ce883fc969896603bf8076933372
Nathaniel Mosco <natemosco@gmail.com>
2019-11-13 09:30:20 -0500

    modified actions to incorporate axiosWithAuth, login component complete, created axiosWithAuth in utils folder

--- friends/src/actions/index.js
+++ friends/src/actions/index.js
@@ -1,4 +1,6 @@
-import axios from "axios";
+// import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+
 
 export const AXIOS_START = "AXIOS_START";
 export const AXIOS_SUCCESS = "AXIOS_SUCCESS";
@@ -7,10 +9,11 @@ export const AXIOS_FAILURE = "AXIOS_FAILURE";
 export const axiosGet = (id) => dispatch => {
     dispatch({ type: AXIOS_START });
     let url = (id) ? `http://localhost:5000/api/friends/${id}` : "http://localhost:5000/api/friends";
-    axios
+    let consoleLogMessage = (id) ? "get request with id" : "get request all"
+    axiosWithAuth()
         .get(url)
         .then(response => {
-            console.log(response);
+            console.log(response, consoleLogMessage);
             dispatch({ type: AXIOS_SUCCESS, payload: response.data })
         })
         .catch(error => {
@@ -19,23 +22,24 @@ export const axiosGet = (id) => dispatch => {
         })
 }
 
-export const newLogin = (user) => dispatch => {
+export const login = (user) => dispatch => {
     dispatch({ type: AXIOS_START });
-    axios
+    axiosWithAuth()
         .post("http://localhost:5000/api/login", user)
         .then(response => {
-            console.log(response);
+            console.log(response, "login response");
             dispatch({ type: AXIOS_SUCCESS, payload: response.data })
+            props.history.push("/")
         })
         .catch(error => {
-            console.log("error from get axios", error);
+            console.log("error from login axios", error);
             dispatch({ type: AXIOS_FAILURE, payload: `${error.response.status} ${error.response.data}` })
         })
 }
 
 export const addUser = (user) => dispatch => {
     dispatch({ type: AXIOS_START });
-    axios
+    axiosWithAuth()
         .post("http://localhost:5000/api/friends", user)
         .then(response => {
             console.log(response);
@@ -49,7 +53,7 @@ export const addUser = (user) => dispatch => {
 
 export const deleteUser = (user) => dispatch => {
     dispatch({ type: AXIOS_START });
-    axios
+    axiosWithAuth()
         .put(`http://localhost:5000/api/friends/:${id}`)
         .then(response => {
             console.log(response);
@@ -63,7 +67,7 @@ export const deleteUser = (user) => dispatch => {
 
 export const updateUser = (user) => dispatch => {
     dispatch({ type: AXIOS_START });
-    axios
+    axiosWithAuth()
         .put(`http://localhost:5000/api/friends/:${id}`, user)
         .then(response => {
             console.log(response);
--- friends/src/components/Login.js
+++ friends/src/components/Login.js
@@ -1,12 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { connect } from 'react-redux';;
+import { login } from "../actions";
 
-export default function Login() {
+
+function Login(props) {
+    const [creds, setCreds] = useState({
+        username: "",
+        password: ""
+    });
+    const handleChanges = (e) => {
+        let target = e.target;
+        setCreds((creds) => {
+            return {
+                ...creds,
+                [target.id]: target.value
+            }
+        })
+    };
+
+    const onSubmit = e => {
+        e.preventDefault();
+        props.login(creds);
+        setCreds({
+            username: "",
+            password: ""
+        })
+    }
+
     return (
+
         <div>
             <form action="submit">
-                <label htmlFor="email">email:</label>
-                <input type="text" />
+                <label htmlFor="username">email:</label>
+                <input type="email" id="username" value={creds.username} placeholder="email is your username" onChange={handleChanges} />
+                <label htmlFor="password">password:</label>
+                <input type="password" id="password" value={creds.password} placeholder="password here" onChange={handleChanges} />
+                <button onClick={onSubmit}>Login</button>
             </form>
         </div>
     )
 }
+export default connect(null, { login })(Login);
